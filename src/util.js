5896caf770ec9a5f023d370e85b5c88bbf1898ea
KUROSHIORI\edgar <edgar135300@gmail.com>
2023-01-21 03:56:26 -0700

    Fixed the animebytes whitelist

--- src/util.js
+++ src/util.js
@@ -195,7 +195,7 @@ const whitelist = [
 
     // Site specific
     /^https:\/\/anidex\.info\/dl\/\d+$/,
-    /^https:\/\/animebytes\.tv\/torrent\/\d+\/download\/$/,
+    /^https:\/\/animebytes\.tv\/torrent\/\d+\/download\/[\w]+$/,
 ];
 
 export const isTorrentUrl = (url) => {
